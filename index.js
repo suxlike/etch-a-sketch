6a15f56353508c12f50cb917c07083266148c3a1
Umut Toker <utoker@gmail.com>
2021-06-18 18:34:58 -0400

    add style

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ function painting() {
   this.style.backgroundColor = "black";
 }
 function clear() {
-  for (i = 0; i < boardSize * boardSize; i++) {
-    cellsArray[i].style.backgroundColor = "whitesmoke";
-  }
-}
-function setSize(e) {
-  let value = e.target.value;
-  console.log(value);
-  document.querySelector(".range-value").textContent = `${value}`;
   while (mainBoard.firstChild) {
     mainBoard.removeChild(mainBoard.firstChild);
-    console.log(mainBoard.firstChild);
+  }
+  makeBoard();
+}
+
+function setSize(e) {
+  let value = e.target.value;
+
+  document.querySelector(".range-value").textContent = `Pixel Size ${value}`;
+  while (mainBoard.firstChild) {
+    mainBoard.removeChild(mainBoard.firstChild);
   }
   boardSize = value;
-  console.log(boardSize);
+
   mainBoard.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`;
   mainBoard.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
   makeBoard();
@@ -25,7 +26,7 @@ let boardSize = 32;
 let clearbtn = document.querySelector(".clear");
 clearbtn.addEventListener("click", clear);
 let mainBoard = document.querySelector(".main-board");
-//
+
 function makeBoard() {
   mainBoard.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`;
   mainBoard.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
@@ -45,23 +46,5 @@ function makeBoard() {
   rangeInput.addEventListener("change", setSize);
 }
 makeBoard();
-let cells = document.querySelectorAll(".main-board div");
-let cellsArray = [...cells];
-
-// mainBoard.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`;
-// mainBoard.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
-
-// for (i = 0; i < boardSize * boardSize; i++) {
-//   mainBoard.appendChild(document.createElement("div"));
-// }
-
-// for (i = 0; i < boardSize * boardSize; i++) {
-//   cellsArray[i].classList.add("cell");
-//   cellsArray[i].classList.add(`cell[${i}]`);
-// }
-// for (i = 0; i < boardSize * boardSize; i++) {
-//   cellsArray[i].addEventListener("mouseover", painting);
-// }
 let rangeInput = document.querySelector(".range-input");
 rangeInput.addEventListener("change", setSize);
-makeBoard();
